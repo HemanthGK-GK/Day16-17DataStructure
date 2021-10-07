82446020ec0de343d939698df6cbf57c2a7fcb92
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-07 17:05:53 +0530

    Hemanth:Find Two strings are Anagrams or not

---
+++ Anagram/Neww.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Anagram
+{
+    class Neww
+    {
+        bool flag = false;
+        public void Check(ArrayList s1,ArrayList s2)
+        {
+            s1.Sort();
+            s2.Sort();
+          
+            for(int i=0;i<s1.Count;i++)
+            {
+                if (s1[i] != s2[i])
+                    flag = false;
+                
+            }
+            flag = true;
+
+        }
+        public void display()
+        {
+            if (this.flag = false)
+            {
+                Console.WriteLine("Not Anagram");
+            }
+            else
+                Console.WriteLine("Anagram string");
+
+        }
+    }
+}
---
+++ Anagram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList s1 = new ArrayList();
+            s1.Add('R');
+            s1.Add('A');
+            s1.Add('C');
+            s1.Add('E');
+            ArrayList s2 = new ArrayList();
+            s2.Add('C');
+            s2.Add('A');
+            s2.Add('R');
+            s2.Add('E');
+
+            Neww obj = new Neww();
+            obj.Check(s1, s2);
+            obj.display();
+           
+
+        }
+    }
+}
