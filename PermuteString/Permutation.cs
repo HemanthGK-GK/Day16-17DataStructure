748dd2a5cd8528554582ae672ebb6204994f19fd
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-07 17:07:28 +0530

    Hemanth : Display the Permutation of string

---
+++ PermuteString/Permutation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermuteString
+{
+    class Permutation
+    {
+        public void permute(string str,string words)
+        {
+            if (str.Length == 0)
+            {
+                Console.Write(words + "  ");
+                return;
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+                String left_str = str.Substring(0, i);
+                String right_str = str.Substring(i + 1);
+                String rest = left_str + right_str;
+                permute(rest, words + ch);
+            }
+        }
+    }
+}
---
+++ PermuteString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PermuteString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            string words=" ";
+            Console.WriteLine("Enter the string : ");
+            str = Console.ReadLine();
+            Permutation ob = new Permutation();
+            Console.WriteLine("All possible strings are : ");
+            ob.permute(str, words);
+        }
+    }
+}
