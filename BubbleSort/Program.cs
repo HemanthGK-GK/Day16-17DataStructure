09dac6d8f1c7a935c803d17540c66e0c6cc18046
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-09 17:04:28 +0530

    Hemanth : Sorting the given values using Bubble Sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 25, 63, 14, 79, 31 };
+            int n = 5, i, j, temp;
+
+            Console.WriteLine("Before sorting");
+            foreach (int no in array)
+            {
+                Console.WriteLine(no);
+            }
+
+            for (i=0;i<n-1;i++)
+            {
+                for (j=0;j<n-1;j++)
+                {
+                    if(array[j]>array[j+1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("After sorting");
+            foreach (int no in array)
+            {
+                Console.WriteLine(no);
+            }
+
+        }
+    }
+}
