94ee1ac306d214e2c52425d972c727be917e8cb7
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-09 18:43:08 +0530

    Hemanth : Dispalay the given message format using REGEX

---
+++ MessageFormat/Format.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MessageFormat
+{
+    class Format
+    {
+        public string RegexName = "^[A-Z]{1}[a-z]{2,}$";
+        public string RegexFullName = "^[A-Z]{1}[a-z]{3,}$";
+        public string RegexPhNum = "^[6-9]{1}[0-9]{9}$";
+        public bool ValidateName(string name)
+        {
+            return Regex.IsMatch(name, RegexName);
+        }
+        public bool ValidateFname(string fullname)
+        {
+            return Regex.IsMatch(fullname, RegexFullName);
+        }
+
+        public bool ValidateNum(string PhNum)
+        {
+            return Regex.IsMatch(PhNum, RegexPhNum);
+        }
+
+    }
+}
---
+++ MessageFormat/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MessageFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string fullname ;
+            string PhNum ;
+            bool r1,r2,r3;
+
+            
+            Format obj = new Format();
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter FullName");
+            fullname = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number");
+            PhNum = Console.ReadLine();
+
+
+
+            r1 = obj.ValidateName(name);
+            Console.WriteLine(r1);
+
+            r2= obj.ValidateFname(fullname);
+            Console.WriteLine(r2);
+
+            r3= obj.ValidateNum(PhNum); 
+            Console.WriteLine(r3);
+
+            if (r1 && r2 && r3 == true)
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Hello {0},We have your full name as {1} in our system.\n your contact number is 91 - {2}.\n Please, let us know in case of any clarification Thank you BridgeLabz", name, fullname, PhNum);
+            }
+            else
+                Console.WriteLine("Enter Valid Input");
+
+        }
+    }
+}
